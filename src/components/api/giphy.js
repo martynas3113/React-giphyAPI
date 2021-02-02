3bda371236a9b647b42e63d480c20c4ac60ade54
martynas3113 <martynas.pazusis@gmail.com>
2021-02-02 21:15:56 +0200

    validations

--- src/components/api/giphy.js
+++ src/components/api/giphy.js
@@ -1,71 +1,71 @@
 import axios from 'axios';
-import React , { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './giphy.scss';
-import Loader from '../loader/loader';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Container } from 'react-bootstrap';
 
 
 
 const Giphy = () => {
 
-    const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
-    const [data, setData] = useState([]); 
+    const [data, setData] = useState([]);
     const [search, setSearch] = useState("");
-    // const [currentPage, setCurrentPage] = useState(1);
-    // const [itemsPerPage, setItemsPerPage] = useState(12);
-    // const lastItemIndex = currentPage * itemsPerPage;
-    // const firstItemIndex = lastItemIndex - itemsPerPage;
-     
+    const [currentPage, setCurrentPage] = useState(0);
+    const [validated, setValidated] = useState(true);
 
     const fetchGif = () => {
+        formValidation();
         const fetchData = async () => {
             setIsError(false);
-            setIsLoading(true);
-            try{
-                const results = await axios("https://api.giphy.com/v1/gifs/trending", {
-                params: {
-                    api_key: "X4hYOXoS6tKkZfdVRY4Nwwr00LIFMw5d",
-                    limit: 12,
-                    offset: 12
+            try {
+                const results = await axios(`https://api.giphy.com/v1/gifs/search`, {
+                    params: {
+                        api_key: "X4hYOXoS6tKkZfdVRY4Nwwr00LIFMw5d",
+                        limit: 12,
+                        offset: currentPage * 12,
+                        q: search
                     }
-                },);
-                setData(results.data.data);
-                console.log(results.data.data)
+                });
 
-            }catch (err) {
+                setData((prevState) => [
+                    ...prevState,
+                    ...results.data.data
+                ])
+
+                setCurrentPage(currentPage + 1)
+
+            } catch (err) {
                 setIsError(true);
                 setTimeout(() => {
                     setIsError(false);
                 }, 5000);
 
             }
-            setIsLoading(false);
         }
 
         fetchData()
     }
     useEffect(() => {
-       fetchGif();
-    },[]);
+        fetchGif();
+    }, []);
 
     const renderGifs = () => {
-        if(isLoading){
-            return <Loader/> 
-        }
-        return data.map(item => {
-            return (
-                <div key={item.id} className="container gif">
-                    <img alt={item.slug} src={item.images.downsized.url}/>
-                </div>
-            )
-            
-        })
-    
+        return <InfiniteScroll className="wrapper" dataLength={data.length} next={() => fetchGif()} hasMore={true}>
+            {data.map((item, idx) => {
+                return (
+                    <Container key={idx} className="container gif">
+                        <img alt={item.slug} src={item.images.downsized.url} />
+                    </Container>
+                )
+            })}
+        </InfiniteScroll>
+
     }
 
     const renderError = () => {
-        if(isError){
-            return(
+        if (isError) {
+            return (
                 <div className="alert alert-danger alert-dismissible fade show" role="alert">
                     Unable to get Gifs, please try again in a few minutes
                 </div>
@@ -77,51 +77,67 @@ const Giphy = () => {
         setSearch(e.target.value)
     }
 
-    const handleSubmit = async e => {
-        e.preventDefault();
-        setIsError(false);
-        setIsLoading(true)
-        try{
-            const results = await axios("https://api.giphy.com/v1/gifs/search", {
-            params: {
-                api_key: "X4hYOXoS6tKkZfdVRY4Nwwr00LIFMw5d",
-                q: search,
-                limit: 12
-            }
-        });
-        setData(results.data.data);
-        } catch (err){
-            setIsError(true);
-            setTimeout(() => {
-                setIsError(false);
-            }, 5000);
+    const formValidation = () => {
+        let isValid = false;
+        const nameValid = /[^A-Za-z0-9]+/g;
+        if (nameValid.test(search)) {
+            isValid = true;
+            setValidated(true);
+        } else {
+            setValidated(false);
         }
-        setIsLoading(false);
+        return isValid;
+
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const isValid = formValidation();
+
+    }
+
+    const handleSubmit = async e => {
+        if (!formValidation()) {
+            e.preventDefault();
+            formValidation();
+            setIsError(false);
+            try {
+                const results = await axios("https://api.giphy.com/v1/gifs/search", {
+                    params: {
+                        api_key: "X4hYOXoS6tKkZfdVRY4Nwwr00LIFMw5d",
+                        q: search,
+                        limit: 12,
+                    }
+                });
+
+                setData(results.data.data)
+
+            } catch (err) {
+                setIsError(true);
+                setTimeout(() => {
+                    setIsError(false);
+                }, 5000);
+
+            }
+        }
+        
+
     };
 
-    return(
+    return (
         <div className="wrap">
             {renderError()}
-            <form className="input-form form-inline justify-content-center">
-                <input value={search} onChange={handleSearch} type="text" placeholder="Search" className="form-control"/>
+            <form onSubmit={onSubmit} className="input-form form-inline justify-content-center">
+                <input value={search} autoFocus required onChange={handleSearch} type="text" placeholder="Search for GIFS" className="form-control animate" />
                 <button onClick={handleSubmit} type="submit" className="btn btn-primary">Go</button>
             </form>
-        <div className="container gifs">
-        {renderGifs()}
+            <div style={{ right: validated ? 0 : `${-20}%` }} className={validated ? "error active " : "error"}>Please input only characters and numbers</div>
+            <div className="container gifs">
+                {renderGifs()}
+            </div>
         </div>
-        </div>
-        ) 
-    
+    )
+
 }
 
 export default Giphy
-
-
-// return data.map(item => {
-//     return (
-//         <div key={item.id} className="container gif">
-//             <img alt={item.slug} src={item.images.downsized.url}/>
-//         </div>
-//     )
-    
-// })
